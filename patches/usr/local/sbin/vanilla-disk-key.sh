5cafb0ddddc72c220c688eb24b92d85433baa3bc
Ben Armston <ben.armston@alces-software.com>
2022-09-16 15:46:01 +0100

    Remove previous attempt at bootstrapping mia

--- patches/usr/local/sbin/vanilla-disk-key.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dev=$([ -d /sys/class/net/eth0 ] && echo /sys/class/net/eth0 || ls -d /sys/class/net/en* | head -n 1)
-idx=$(printf %02d $(expr $(printf '%d' 0x`md5sum ${dev}/address | cut -c1-2`) % 20))
-echo /data/private/share/vanilla/$idx.key
--- scripts/stage-2/prepare-chroot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MOUNT_POINT=/mnt/staging
-
-mkdir -p "${MOUNT_POINT}"/{dev,proc,sys}
-mount -o bind /dev "${MOUNT_POINT}"/dev
-mount -t proc none "${MOUNT_POINT}"/proc
-mount -t sysfs none "${MOUNT_POINT}"/sys
