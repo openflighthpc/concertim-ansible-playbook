a12c670130719e4aa0f4126708dcce3082ca4702
Ben Armston <ben.armston@alces-software.com>
2023-03-10 12:29:01 +0000

    Don't have FTSW/setup.sh configure certificates
    We weren't using them anyway.

--- ansible/roles/configure-vanilla/files/setup.sh
+++ ansible/roles/configure-vanilla/files/setup.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-function relocate {
-    # move certs to /etc/ssl
-    ct='/data/private/share/etc/concurrent-thinking'
-    if [ "$pver" != "3" ]; then
-      mv $ct/ssl/certs/*.$1_crt.pem /etc/ssl/certs
-      mv $ct/ssl/private/*.$1_key.pem /etc/ssl/private
-      mv $ct/ssl/certs/concertim-CA_crt.pem /etc/ssl/certs
-      mv $ct/ssl/certs/phoenix-CA_crt.pem /etc/ssl/certs
-      cat /etc/ssl/certs/concertim-CA_crt.pem /etc/ssl/certs/phoenix-CA_crt.pem > /etc/ssl/certs/cacerts.pem
-      # move asc and yml files to /etc/concurrent-thinking/appliance
-      mv $ct/ssl/gpg/* $ct/appliance
-    else
-      mv $ct/ssl/*.$1_crt.pem /etc/ssl/certs
-      mv $ct/ssl/private/*.$1_key.pem /etc/ssl/private
-      mv $ct/ssl/concurrent-CA_crt.pem /etc/ssl/certs
-      mv $ct/ssl/phoenix-CA_crt.pem /etc/ssl/certs
-      cat /etc/ssl/certs/concertim-CA_crt.pem /etc/ssl/certs/phoenix-CA_crt.pem > /etc/ssl/certs/cacerts.pem
-      # move asc and yml files to /etc/concurrent-thinking/appliance
-      mv $ct/ssl/*.yml $ct/appliance
-      mv $ct/ssl/*.asc $ct/appliance
-    fi
-    chown www-data $ct/appliance/release.yml
-}
 
 if [ $UID != 0 ]; then
     echo "Must be run as root"
@@ -41,26 +18,6 @@ case $STAGE in
     # ------------------------------------------------------------
     reset)
 
-    # # Remove ssl certificates (from certificates.tgz)
-    for cert in $(tar tzf $src/certificates.tgz | grep '.pem$')
-    do
-        rm -f /etc/ssl/certs/$cert
-        rm -f /etc/ssl/private/$cert
-    done
-    rm -f /etc/ssl/certs/apache_crt.pem
-    rm -f /etc/ssl/private/apache_key.pem
-
-    # # Remove non certificate contents of certificates.tzg
-    rm -f $src/release.yml.asc
-
-    # # Remove contents of appliance_config.dat (aka appliance-config.tgz)
-    rm -f $src/security-pack.tgz
-    rm -rf $src/bootstrap
-
-    # # Remove contents of security-pack.tgz
-    rm -f $src/security.yml
-    rm -f $src/certificates.tgz
-    rm -f $src/pack.yaml
 
 
     ;;
@@ -83,31 +40,11 @@ case $STAGE in
     # ------------------------------------------------------------
     security_prep)
 
-    cd $src
-    tar xvzf $src/appliance-config.tgz
-    tar xvzf $src/security-pack.tgz
 
-    cd $ct/ssl
-    tar xvzf $src/certificates.tgz
-    chown www-data $ct/ssl/private/*
-
-    DOMAIN=`cat $ct/ssl/gpg/release.yml | grep domain_name | awk '{print $2}'`
-    pver=`/opt/ruby-1.8/bin/ruby -e "require 'rubygems'; require 'yaml'; info = YAML.load_file('/data/private/share/etc/concurrent-thinking/appliance/release.yml'); puts \"#{info['pack_version']}\""`
-    relocate $DOMAIN $pver
 	;;
 
     # ------------------------------------------------------------
     security)
-
-    HOST=$1
-    DOMAIN=$2
-
-    # we work out the domain dynamically here 
-    # since if there's anything wrong with the value in ApplianceNetworkInterface then we'll never restore a backup
-    DOMAIN=`cat $ct/appliance/release.yml | grep domain_name | awk '{print $2}'`
-
-    ln -snf /etc/ssl/certs/${HOST}.${DOMAIN}_crt.pem /etc/ssl/certs/apache_crt.pem
-    ln -snf /etc/ssl/private/${HOST}.${DOMAIN}_key.pem /etc/ssl/private/apache_key.pem
 
 	;;
 
