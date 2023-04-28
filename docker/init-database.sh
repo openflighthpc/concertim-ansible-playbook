01e3472461dd0b56de6f4e6c0bf9a8a3692c536b
Ben Armston <ben.armston@alces-software.com>
2023-04-28 22:46:07 +0100

    Database user fixes
    * Keep the postgres user
    * Configure a www-data user

--- docker/init-database.sh
+++ docker/init-database.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-# createuser --username "$POSTGRES_USER" www-data -S -R -d
+PASSWORD="$(cat /run/secrets/db-password)"
+
+createuser --username "$POSTGRES_USER" www-data -S -R -d
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	ALTER USER "www-data" WITH PASSWORD '${PASSWORD}';
+EOSQL
